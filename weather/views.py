b774d0dfa1f6e17ebffa5cba2237a4eeebaae0b0
Sebastian Caceres <sebastian.c17@yandex.com>
2021-06-05 02:17:24 -0400

    feat: Progress

--- weather/views.py
+++ weather/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'home.html', {})
+    import json
+    import requests
+    api_request = requests.get("http://api.meteored.cl/index.php?api_lang=cl&localidad=17725&affiliate_id=dypamzcc8893&v=3.0")
+    try:
+        api = json.loads(api_request.content)
+    except Exception as e:
+        api = "Error..."
+
+    return render(request, 'home.html', {'api': api})
 
 def about(request):
     return render(request, 'about.html', {})
